fa44b218a1febf837686140bf73da0312429e92d
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-07 23:18:52 +0900

    Introduce navigation

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator  } from "@react-navigation/stack";
+import IndexScreen from "./src/screens/IndexScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Index">
+        <Stack.Screen name="Index" component={IndexScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>aaa</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
