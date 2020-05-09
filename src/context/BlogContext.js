7f14e9820e90345921f87d49fbd7b8656b9dc93c
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-09 23:45:11 +0900

    Add delete function

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,9 +4,12 @@ import createDataContext from "./createDataContext";
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'add_blogpost':
-      return [...state, { title: `Blog Post #${state.length + 1}`}];
+      return [...state, {
+        id: Math.floor(Math.random() * 999999),
+        title: `Blog Post #${state.length + 1}`}];
     case 'edit_blogpost':
     case 'delete_blogpost':
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     default:
       return state;
   }
@@ -18,4 +21,15 @@ const addBlogPost = (dispatch) => {
   };
 };
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost: addBlogPost }, []);
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blogpost', payload: id });
+  };
+};
+
+export const { Context, Provider } = createDataContext(blogReducer,
+  {
+    addBlogPost: addBlogPost,
+    deleteBlogPost: deleteBlogPost,
+  },
+  []);
