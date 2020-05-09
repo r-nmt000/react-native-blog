4842689f07bac00728fb9a232bf25f1d25fe5500
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-09 17:31:35 +0900

    Use Reducer instead of Stae

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}`}];
+    case 'edit_blogpost':
+    case 'delete_blogpost':
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
   const addBlogPost = () => {
-    setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}]);
+    dispatch({ type: 'add_blogpost' });
   };
 
   return (
