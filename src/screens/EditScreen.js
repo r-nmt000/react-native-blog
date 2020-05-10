d8376ad9dbf2f5e59a1038aa02d122c6e698e8f7
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-10 11:59:39 +0900

    feat: extract common component among create and edit screen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,19 +1,15 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({route}) => {
   const { state } = useContext(Context);
   const blogPost = state.find((blogPost) => blogPost.id === (route.params?.id ?? 0));
+
   return (
-    <View>
-      <Text>Edit Screen - {route.params?.id ?? 0}</Text>
-      <Text>Title</Text>
-      <TextInput value={blogPost.title} />
-      <Text>Content</Text>
-      <TextInput value={blogPost.content} />
-    </View>
-  );
+    <BlogPostForm />
+  )
 };
 
 const styles = StyleSheet.create({});
