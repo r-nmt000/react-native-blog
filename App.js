92b5c64f834d539bc2c2b693cccfd21f85d1ff73
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-10 10:34:11 +0900

    Add create screen component and its routing

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator  } from "@react-navigation/stack";
 import IndexScreen from "./src/screens/IndexScreen";
 import {Provider} from "./src/context/BlogContext";
 import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
+import { Feather } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 
@@ -12,8 +14,20 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Index">
-        <Stack.Screen name="Index" component={IndexScreen}/>
+        <Stack.Screen
+          name="Index"
+          component={IndexScreen}
+          options={({navigation}) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+                  <Feather name="plus" size={30}/>
+                </TouchableOpacity>
+              )
+            }
+          )}
+          />
         <Stack.Screen name="Show" component={ShowScreen}/>
+        <Stack.Screen name="Create" component={CreateScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
